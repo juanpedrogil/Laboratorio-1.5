7ecf1844967205fa2bd8b597e3f93bb60e5f2049
Juan Pedro Gil Llanos <jupegilllanos@ittepic.edu.mx>
2018-02-25 11:41:36 -0600

    Terminado

--- app/src/main/java/com/debug/prueba/aplicacion/juanpedrog/laboratorio15/Adapter.java
+++ app/src/main/java/com/debug/prueba/aplicacion/juanpedrog/laboratorio15/Adapter.java
@@ -1,30 +1,41 @@
 package com.debug.prueba.aplicacion.juanpedrog.laboratorio15;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by juanpedrog on 22/02/18.
  */
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ListaViewHolder> {
-
+    String[][] datos;
+    public Adapter(String[][] datos){
+        this.datos=datos;
+    }
     @Override
     public ListaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+        ListaViewHolder lista=new ListaViewHolder(view);
+        return lista;
     }
 
     @Override
     public void onBindViewHolder(ListaViewHolder holder, int position) {
-
+        holder.lblNombre.setText(datos[position][0]);
+        holder.lblControl.setText(datos[position][1]);
+        holder.lblCarrera.setText(datos[position][2]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return datos.length;
     }
 
     public static class ListaViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/debug/prueba/aplicacion/juanpedrog/laboratorio15/MainActivity.java
+++ app/src/main/java/com/debug/prueba/aplicacion/juanpedrog/laboratorio15/MainActivity.java
@@ -2,12 +2,29 @@ package com.debug.prueba.aplicacion.juanpedrog.laboratorio15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String datos[][]={{"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"},
+            {"Juan Pedro Gil Llanos","14400941","ISC"}};
+    Adapter adapter;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.lista);
+        adapter=new Adapter(datos);
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
     }
 }
